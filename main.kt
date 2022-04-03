d8ce5e059aae83b077224e43581e3204eaddb3a5
hiro881213 <nikep.godbard@gmail.com>
2022-04-04 01:49:20 +0900

    型サフィックス

--- main.kt
+++ main.kt
@@ -1,3 +1,8 @@
+/**
+ * 
+ * Main関数
+ * 
+ */
 fun main() {
     println("Hello World!")
 
@@ -5,8 +10,18 @@ fun main() {
     var b = 1_000
     var c = 0x3E8
 
+
+    println("変数の利用")
     println(a)
     println(b)
     println(c)
+
+    println("型サフィックス")
+    var d = 10L
+    var e = 10F
+
+    println(a::class)
+    println(d::class)
+    println(e::class)
 
 }
